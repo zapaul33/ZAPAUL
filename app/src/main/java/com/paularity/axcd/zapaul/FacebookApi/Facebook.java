469ee2fd9f29b3d60491b46cdfedda9a01d0e3fb
Christian Decembrana <work.christiandecembrana@gmail.com>
2018-11-26 18:52:34 +0800

    first

---
+++ app/src/main/java/com/paularity/axcd/zapaul/FacebookApi/Facebook.java
@@ -0,0 +1,8 @@
+package com.paularity.axcd.zapaul.FacebookApi;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class Facebook
+{
+}
---
+++ app/src/main/java/com/paularity/axcd/zapaul/MainActivity.java
@@ -0,0 +1,135 @@
+package com.paularity.axcd.zapaul;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    CallbackManager callbackManager;
+    TextView txtEmail, txtBirthday, txtFriends;
+    ProgressDialog mDialog;
+    ImageView imgAvatar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        txtBirthday = (TextView) findViewById(R.id.txtBirthday);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtFriends = (TextView) findViewById(R.id.txtFriends);
+
+        imgAvatar = (ImageView) findViewById(R.id.avatar);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends") );
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("Retrieving data...");
+                mDialog.show();
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken() , new GraphRequest.GraphJSONObjectCallback()
+                {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response)
+                    {
+                        mDialog.dismiss();
+                        Log.e("response", response.toString());
+                        getData( object );
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields","id,email,birthday,friends");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel()
+            {
+
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+
+            }
+        });
+
+        if(AccessToken.getCurrentAccessToken() != null)
+        {
+            txtEmail.setText( AccessToken.getCurrentAccessToken().getUserId() );
+        }
+    }
+
+    private void getData( JSONObject object )
+    {
+        try
+        {
+            URL profile_picture = new URL("https://graph.facebook.com/" + object.getString("id")+"/picture?width:250&height=250");
+
+            Picasso.with(this).load( profile_picture.toString() ).into(imgAvatar);
+
+            txtEmail.setText(object.getString("email"));
+            txtBirthday.setText(object.getString("birthday"));
+            txtFriends.setText("Friends: " + object.getJSONObject("friends").getJSONObject("summary").getString("total_count") );
+
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
